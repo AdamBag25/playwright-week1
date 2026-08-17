59a6343f05fe4aa563773fcdbad8b98f3c2cc033
adambag25 <adambagalwa@gmail.com>
2026-08-17 21:23:14 +0100

    Add third test: get started link opens installation page

---
+++ tests/third.spec.ts
@@ -0,0 +1,8 @@
+import{test,expect} from '@playwright/test';
+
+test ('get started link opens installation page', async ({page}) => {
+   await page.goto('https://playwright.dev/');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+ });
+
